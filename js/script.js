14ff2ed9167824b5e7d4b65f8be9c8193cc40a42
codercodemanish <72806831+codercodemanish@users.noreply.github.com>
2021-11-20 00:04:29 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// alert("welcome");
+
+function openMenu(){
+    document.getElementById("navbar").style.left = "0px";
+}
+
+function closeMenu(){
+    document.getElementById("navbar").style.left = "-300px";
+}
+
